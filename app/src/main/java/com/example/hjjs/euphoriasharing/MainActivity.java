f10d66fc3d4ddbb3e092eced5b7b494137568465
HJJS <saumildedhia@gmail.com>
2017-09-29 22:12:16 +0530

    initial checking

--- app/src/main/java/com/example/hjjs/euphoriasharing/MainActivity.java
+++ app/src/main/java/com/example/hjjs/euphoriasharing/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_gallery:
+                    mTextMessage.setText("gallery");
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_folder:
+                    mTextMessage.setText("folder");
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_share:
+                    mTextMessage.setText("share");
                     return true;
             }
             return false;
